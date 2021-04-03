f928bc1da9ff191a73fecbfc49493a028d4a9e76
AnithaPal <anithaspark@gmail.com>
2021-04-03 15:28:33 -0400

    initialized repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { HyperfluxComponent } from './hyperflux-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HyperfluxComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HyperfluxComponent]
 })
 export class AppModule { }
---
+++ src/app/hyperflux-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hyperflux-app',
+  template: `<h1> Hyperflux </h1>`,
+  styleUrls: ['./hyperflux-app.component.scss']
+})
+export class HyperfluxComponent {
+  title = 'hyperflux-field-monitoring-platform';
+}
