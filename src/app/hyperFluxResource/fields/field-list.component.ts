38f2068037e97aaf0aef06b1c623caf49ef07675
AnithaPal <anithaspark@gmail.com>
2021-04-11 14:00:49 -0400

    Added style to field component

--- src/app/hyperFluxResource/fields/field-list.component.ts
+++ src/app/hyperFluxResource/fields/field-list.component.ts
@@ -13,14 +13,23 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 })
 
 export class FieldListComponent implements OnInit  {
-  fields: IField[];
+  field: IField;
   faCircle = faCircle;
+  aperturePercentage: number;
+  vsPercentage: number;
+
 
   constructor(private hyperFluxService: HyperFluxService, private route: ActivatedRoute){}
 
   ngOnInit(){
-    this.fields = this.route.snapshot.data.fields;
-    console.log('fields is ' + this.fields);
+    this.field = this.route.snapshot.data.fields[0];
+    this.aperturePercentage = this.calculatePercentage(this.field.gfaBandwidthUsage, this.field.gfaBandwidthLimit);
+    this.vsPercentage = this.calculatePercentage(this.field.vSpaceUsage, this.field.vSpaceLimit);
+    console.log('fields is ' + this.field);
+  }
+
+  calculatePercentage(usage: number, limit: number): number {
+    return Math.round(usage / limit * 100);
   }
 
 }
