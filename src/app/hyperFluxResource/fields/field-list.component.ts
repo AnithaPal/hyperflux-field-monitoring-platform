ae799ba447dc45feecefaa1aa8d68f0a7ac42aff
AnithaPal <anithaspark@gmail.com>
2021-04-11 11:29:32 -0400

    Add field list component skeleton

--- src/app/hyperFluxResource/fields/field-list.component.ts
+++ src/app/hyperFluxResource/fields/field-list.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { HyperFluxService } from '../hyper-flux-service';
 import { ActivatedRoute } from '@angular/router';
 import { IField } from '../field.model';
+import {SidebarComponent} from '../../nav/sidebar/sidebar.component';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-filed-list',
-  templateUrl: './field-list.component.html'
+  templateUrl: './field-list.component.html',
+  styleUrls: ['./field-list.component.scss']
+
 })
 
 export class FieldListComponent implements OnInit  {
   fields: IField[];
+  faCircle = faCircle;
 
   constructor(private hyperFluxService: HyperFluxService, private route: ActivatedRoute){}
 
